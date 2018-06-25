8a54e2305311509810f5e9cec8c506fece218086
at hetao <414189369@qq.com>
2018-06-25 14:52:30 +0800

    改变scratch浏览所用端口号

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const base = {
     devServer: {
         contentBase: path.resolve(__dirname, 'build'),
         host: '0.0.0.0',
-        port: process.env.PORT || 8601
+        port: process.env.PORT || 8888
     },
     output: {
         library: 'GUI',
